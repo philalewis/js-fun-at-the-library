29934296d8ae6704883347cb82cafbdb0d1e5a64
Phil Lewis <phillewis1985@gmail.com>
2021-10-13 20:02:56 -0600

    Pass buildMainCharacter test

--- src/book.js
+++ src/book.js
@@ -2,9 +2,18 @@ function createTitle(title) {
   return `The ${title}`;
 }
 
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  };
+  return character;
+}
+
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
