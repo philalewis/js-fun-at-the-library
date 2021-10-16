7f03193433e6e1017d9992b6eb446ec8cfec96e4
Phil Lewis <phillewis1985@gmail.com>
2021-10-15 20:25:34 -0600

    Pass checkoutBook function tests

--- src/Library.js
+++ src/Library.js
@@ -14,8 +14,15 @@ function addBook(library, book) {
   library.shelves[book.genre].push(book);
 }
 
-function checkoutBook(library, book, genre) {
-  library.shelves.genre.pop()
+function checkoutBook(library, book, shelf) {
+  var message = `You have now checked out ${book} from the ${library.name}`;
+  for (let i = 0; i < library.shelves[shelf].length; i++) {
+    if (book === library.shelves[shelf][i].title) {
+      library.shelves[shelf].splice(i, 1);
+      return message;
+    }
+  }
+  return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
 }
 
 module.exports = {
--- test/Library-test.js
+++ test/Library-test.js
@@ -114,7 +114,7 @@ describe("library.js", function() {
       assert.equal(result2, "You have now checked out Born a Crime from the Denver Public Library")
     });
 
-    it.skip("should only checkout a book if the book is on the shelves", function() {
+    it("should only checkout a book if the book is on the shelves", function() {
       var denverLibrary = createLibrary("Denver Public Library");
 
       var error1 = checkoutBook(denverLibrary, "The Fifth Season", "fantasy");
