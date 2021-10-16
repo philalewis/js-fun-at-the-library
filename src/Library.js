3fd0b545dd2fb278eb8b7d52dd6a55ed427939a0
Phil Lewis <phillewis1985@gmail.com>
2021-10-15 19:46:43 -0600

    Pass tests for first two funcitons

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,25 @@
+function createLibrary(name) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+  }
+  return library;
+}
 
+function addBook(library, book) {
+  library.shelves[book.genre].push(book);
+}
+
+function checkoutBook(library, book, genre) {
+  library.shelves.genre.pop()
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
