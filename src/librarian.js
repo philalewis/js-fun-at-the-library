e8be77d3b5f0b6dc87e812c0066d4d05e07a76f8
Phil Lewis <phillewis1985@gmail.com>
2021-10-16 21:29:08 -0600

    Pass all but last test in librarian.js

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,45 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+    // function createLibrary(name) {
+    //   var library = {
+    //     name: name,
+    //     shelves: {
+    //       fantasy: [],
+    //       fiction: [],
+    //       nonFiction: [],
+    //     }
+    //   }
+    //   return library;
+    // }
+    this.greetPatron = function(patronName, isMorning) {
+      if (isMorning === true) {
+        return `Good morning, ${patronName}!`
+      }
+      return `Hello, ${patronName}!`
+    }
+    this.findBook = function(bookTitle) {
+      for (let i = 0; i < this.library.shelves.fantasy.length; i++) {
+        if (this.library.shelves.fantasy[i].title === bookTitle) {
+          this.library.shelves.fantasy.splice(i, 1);
+          return `Yes, we have ${bookTitle}`
+        }
+      }
+      for (let i = 0; i < this.library.shelves.fiction.length; i++) {
+        if (this.library.shelves.fiction[i].title === bookTitle) {
+          this.library.shelves.fiction.splice(i, 1);
+          return `Yes, we have ${bookTitle}`
+        }
+      }
+      for (let i = 0; i < this.library.shelves.nonFiction.length; i++) {
+        if (this.library.shelves.nonFiction[i].title === bookTitle) {
+          this.library.shelves.nonFiction.splice(i, 1);
+          return `Yes, we have ${bookTitle}`
+        }
+      }
+      return `Sorry, we do not have ${bookTitle}`
+    }
   }
 }
 
