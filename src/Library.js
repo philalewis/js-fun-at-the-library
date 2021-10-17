cb8f6c106966f06e64f865847a0b5a35c789baed
Phil Lewis <phillewis1985@gmail.com>
2021-10-16 21:28:06 -0600

    Refactor Library.js

--- src/Library.js
+++ src/Library.js
@@ -16,17 +16,15 @@ function addBook(library, book) {
 
 function checkoutBook(library, book, shelf) {
   var message = `You have now checked out ${book} from the ${library.name}`;
-  if (library.shelves.shelf.includes(book.title)) {
-    return message;
+  for (let i = 0; i < library.shelves[shelf].length; i++) {
+    if (book === library.shelves[shelf][i].title) {
+      library.shelves[shelf].splice(i, 1);
+      return message;
+    }
   }
   return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
 }
-// for (let i = 0; i < library.shelves[shelf].length; i++) {
-//   if (book === library.shelves.shelf[i].title) {
-//     library.shelves[shelf].splice(i, 1);
-//     return message;
-//   }
-// }
+
 
 module.exports = {
   createLibrary,
